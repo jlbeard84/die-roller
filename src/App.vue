454932c6742a54ac9f38d623dbe9dfc01503f5c7
Josh Beard <josh@jlbeard.com>
2019-03-13 11:19:22 -0400

    Added skeleton for vue project

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+    <div id="app">
+        <h1>{{ msg }}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'app',
+    data () {
+        return {
+            msg: 'Welcome to die roller'
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+    #app {
+        color: #333;
+    }
+</style>
+
+
+
