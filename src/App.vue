d4038b220babbf154298f733b1b36fb2081fb429
Josh Beard <josh@jlbeard.com>
2019-03-13 11:40:29 -0400

    Added material design lib

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,15 @@
 <template>
     <div id="app">
-        <h1>{{ msg }}</h1>
+        <md-app>
+            <md-app-toolbar md-mode="fixed" class="md-primary">
+                <span class="md-title">Die Roller</span>
+            </md-app-toolbar>
+
+            <md-app-content>
+                <h1>{{ msg }}</h1>
+            </md-app-content>
+        </md-app>
+        
     </div>
 </template>
 
@@ -16,9 +25,7 @@ export default {
 </script>
 
 <style lang="scss">
-    #app {
-        color: #333;
-    }
+
 </style>
 
 
--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import Vue from 'vue';
+import VueMaterial from 'vue-material';
 import App from './App.vue';
+
+import 'vue-material/dist/vue-material.min.css';
+
+Vue.use(VueMaterial);
 
 new Vue({
     el: '#app',
